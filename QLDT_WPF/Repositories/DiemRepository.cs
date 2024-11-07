3de59be7bca066e06b4b98064bab184c9ccaa352
V <lytranvinh.work@gmail.com>
2024-11-08 00:39:34 +0700

    [upgrade]: handle get all diem

--- QLDT_WPF/Repositories/DiemRepository.cs
+++ QLDT_WPF/Repositories/DiemRepository.cs
@@ -3,23 +3,67 @@ using System.Linq;
 
 //
 using QLDT_WPF.Data;
+using QLDT_WPF.Dto;
 
 namespace QLDT_WPF.Repositories;
 
 public class DiemRepository
 {
     // Variables
+    private readonly QuanLySinhVienDbContext _context;
 
     // Constructor
+    public DiemRepository()
+    {
+        var connectionString = ConfigurationManager.ConnectionStrings["QuanLySinhVienDbConnection"].ConnectionString;
+        _context = new QuanLySinhVienDbContext(
+            new DbContextOptionsBuilder<QuanLySinhVienDbContext>()
+                .UseSqlServer(connectionString)
+                .Options);
+    }
 
     /**
      * Lay tat ca diem
      */
+    public Task<ApiResponse<List<DiemDto>>> GetAll()
+    {
+        // Query
+        var query = await(
+            from d in _context.Diems
+            join lhp in _context.LopHocPhans
+                on d.IdLopHocPhan equals lhp.IdLopHocPhan
+            join mon in _context.MonHocs
+                on lhp.IdMonHoc equals mon.IdMonHoc
+            select new DiemDto
+            {
+                IdDiem = d.IdDiem,
+                IdSinhVien = d.IdSinhVien,
+                IdLopHocPhan = d.IdLopHocPhan,
+                IdMon = mon.IdMonHoc,
 
+                DiemQuaTrinh = d.DiemQuaTrinh,
+                DiemKetThuc = d.DiemKetThuc,
+                DiemTongKet = d.DiemTongKet,
+                LanHoc = d.LanHoc,
+                TenMonHoc = mon.TenMonHoc,
+                TenLopHocPhan = lhp.TenHocPhan,
+            }
+        ).ToListAsync();
 
+        // Directly return the JSON result
+        return new ApiResponse<List<DiemDto>>
+        {
+            Status = true,
+            Message = "Success",
+            StatusCode = 200,
+            Data = query
+        };
+    }
+    
     /**
      * Lay diem by id
      */
+    
 
     /**
      * Lay diem by id sinh vien
@@ -52,10 +96,10 @@ public class DiemRepository
     /**
      * Nhập điểm cho sinh viên
      */
-    
+
     /**
      * Nhap 'List' Diem Sinh Vien
      */
 
-    
+
 }
