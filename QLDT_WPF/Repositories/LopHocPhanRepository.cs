9fd6a774add7f87235bfd59870a7137d48d6f7db
V <lytranvinh.work@gmail.com>
2024-11-07 17:31:24 +0700

    [upgrade]: todo cmp handle db lop hoc phan

---
+++ QLDT_WPF/Repositories/LopHocPhanRepository.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Linq;
+
+//
+using QLDT_WPF.Data;
+
+namespace QLDT_WPF.Repositories;
+
+public class LopHocPhanRepository
+{
+    // Variables
+
+    // Constructor
+
+    /**
+     * Lay tat ca lop hoc phan
+     */
+
+
+    /**
+     * Lay lop hoc phan by id
+     */
+
+    /**
+     * Lay lop hoc phan by id
+     */
+
+
+    /**
+     * Sua thong tin lop hoc phan
+     */
+
+
+    /**
+     * Them lop hoc phan
+     */
+
+
+    /**
+     * Xoa lop hoc phan By Id 
+     */
+
+    /** 
+     * Get lop hoc phan cua sinh vien tu id
+     */
+    
+    /** 
+     * Get lop hoc phan cua giao vien tu id
+     */
+
+    /**
+     * Get lop hoc phan tu id mon hoc
+     */
+
+    /**
+     * Thay doi thoi gian lop hoc phan 
+     */
+
+    /**
+     * Thêm thời gian cho lớp học phần
+     */
+
+    
+}
