a792e288910d5199d83e713150c15d8942511f8f
V <lytranvinh.work@gmail.com>
2024-11-07 17:27:24 +0700

    [upgrade]: todo cmp handle db diem

---
+++ QLDT_WPF/Repositories/DiemRepository.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Linq;
+
+//
+using QLDT_WPF.Data;
+
+namespace QLDT_WPF.Repositories;
+
+public class DiemRepository
+{
+    // Variables
+
+    // Constructor
+
+    /**
+     * Lay tat ca diem
+     */
+
+
+    /**
+     * Lay diem by id
+     */
+
+    /**
+     * Lay diem by id sinh vien
+     */
+
+
+    /**
+     * Sua thong tin diem
+     */
+
+
+    /**
+     * Them diem
+     */
+
+
+    /**
+     * Xoa diem By Id 
+     */
+
+    /**
+     * Trả Về Danh Sách Điểm Của Sinh Viên Với Lần Thi Cuối Cùng, 
+     * Có Điểm Tổng Kết >= 7, Chưa Có Trong Nguyện Vọng
+     */
+
+    /**
+     * Lay diem by id lop hoc phan
+     */
+
+    /**
+     * Nhập điểm cho sinh viên
+     */
+    
+    /**
+     * Nhap 'List' Diem Sinh Vien
+     */
+
+    
+}
