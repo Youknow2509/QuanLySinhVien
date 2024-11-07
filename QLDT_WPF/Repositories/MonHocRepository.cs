c538d3f613f0d1cd08649a468cb0759c0de9b77e
V <lytranvinh.work@gmail.com>
2024-11-07 17:33:02 +0700

    [upgrade]: todo cmp handle db lop hoc mon hoc

---
+++ QLDT_WPF/Repositories/MonHocRepository.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+
+//
+using QLDT_WPF.Data;
+
+namespace QLDT_WPF.Repositories;
+
+public class MonHocRepository
+{
+    // Variables
+
+    // Constructor
+
+    /**
+     * Lay tat ca mon hoc
+     */
+
+
+    /**
+     * Lay mon hoc by id
+     */
+
+    /**
+     * Lay mon hoc by id
+     */
+
+
+    /**
+     * Sua thong tin mon hoc
+     */
+
+
+    /**
+     * Them mon hoc
+     */
+
+
+    /**
+     * Xoa mon hoc By Id 
+     */
+
+    /**
+     * Get data mon hoc for giao vien with id giao vien
+     */
+    
+}
