1064c31df302d5e1d967f120065c775dbb1591b2
V <lytranvinh.work@gmail.com>
2024-11-07 17:24:10 +0700

    [upgrade]: todo cmp handle db chuong trinh hoc

---
+++ QLDT_WPF/Repositories/ChuongTrinhHocRepository.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+
+//
+using QLDT_WPF.Data;
+
+namespace QLDT_WPF.Repositories;
+
+public class ChuongTrinhHocRepository
+{
+    // Variables
+
+    // Constructor
+
+    /**
+     * Lay tat ca chuong trinh hoc
+     */
+
+
+    /**
+     * Lay chuong trinh hoc by id
+     */
+
+    /**
+     * Lay chuong trinh hoc by id sinh vien
+     */
+
+
+    /**
+     * Sua thong tin chuong trinh hoc
+     */
+
+
+    /**
+     * Them chuong trinh hoc
+     */
+
+
+    /**
+     * Xoa chuong trinh hoc By Id 
+     */
+
+    /**
+     * Lay ra danh sach mon hoc co va khong co trong chuong trinh hoc by id chuong trinh hoc
+     */
+
+    /**
+     * Xoa mon hoc khoi chuong trinh hoc
+     */
+
+    /**
+     * Them mon hoc vao chuong trinh hoc
+     */
+
+    /**
+     * Cap nhat ten chuong trinh hoc
+     */
+    
+}
