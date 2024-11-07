73557a0afe4c3b334c46940afef0462a7fd3d009
V <lytranvinh.work@gmail.com>
2024-11-07 17:36:38 +0700

    [upgrade]: todo cmp handle db nguyen vong giao vien

---
+++ QLDT_WPF/Repositories/NguyenVongGiaoVienRepository.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+
+//
+using QLDT_WPF.Data;
+
+namespace QLDT_WPF.Repositories;
+
+public class NguyenVongGiaoVienRepository
+{
+    // Variables
+
+    // Constructor
+
+    /**
+     * Lay tat ca nguyen vong cua giao vien
+     */
+
+
+    /**
+     * Lay nguyen vong cua giao vien by id
+     */
+
+    /**
+     * Lay nguyen vong cua giao vien by id
+     */
+
+
+    /**
+     * Sua thong tin nguyen vong cua giao vien
+     */
+
+
+    /**
+     * Them nguyen vong cua giao vien
+     */
+
+
+    /**
+     * Xoa nguyen vong cua giao vien By Id 
+     */
+
+    /**
+     * Lay nguyen vong by id giao vien
+     */
+
+    /**
+     * Chap nhan nguyen vong by id
+     */
+
+    /**
+     * Tu choi nguyen vong by id
+     */
+    
+}
