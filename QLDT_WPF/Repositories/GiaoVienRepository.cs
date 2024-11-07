0e5318bcbe8b2f6b48a4bd7b8d6cea2052f6dcda
V <lytranvinh.work@gmail.com>
2024-11-07 17:19:17 +0700

    [upgrade]: todo cmp handle db giao vien

---
+++ QLDT_WPF/Repositories/GiaoVienRepository.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+
+//
+using QLDT_WPF.Data;
+
+namespace QLDT_WPF.Repositories;
+
+public class GiaoVienRepository
+{
+    // Variables
+
+    // Constructor
+
+    /**
+     * Lay tat ca giao vien
+     */
+
+
+    /**
+     * Lay giao vien by id
+     */
+
+
+    /**
+     * Sua thong tin giao vien
+     */
+
+
+    /**
+     * Them giao vien
+     */
+
+
+    /**
+     * Xoa giao vien By Id 
+     */
+
+
+    /**
+     * Admin Xử Lí Cập Nhập Mật Khẩu Giáo Viên
+     */
+
+    /**
+     * Xử Lí Cập Nhập Mật Khẩu Giáo Viên - Từ Chính Giáo Viên
+     */
+
+    
+}
