c4a36895ff8a817091aee7bf657fbb64fa8d37f6
V <lytranvinh.work@gmail.com>
2024-11-08 00:42:12 +0700

    [upgeade]: handle get DiemDto by id diem

--- QLDT_WPF/Repositories/DiemRepository.cs
+++ QLDT_WPF/Repositories/DiemRepository.cs
@@ -63,7 +63,40 @@ public class DiemRepository
     /**
      * Lay diem by id
      */
-    
+    public Task<ApiResponse<DiemDto>> GetById(string id)
+    {
+         // Query
+        var query = await (
+            from d in _context.Diems
+            join lhp in _context.LopHocPhans
+                on d.IdLopHocPhan equals lhp.IdLopHocPhan
+            join mon in _context.MonHocs
+                on lhp.IdMonHoc equals mon.IdMonHoc
+            where d.IdDiem == id
+            select new DiemDto
+            {
+                IdDiem = d.IdDiem,
+                IdSinhVien = d.IdSinhVien,
+                IdLopHocPhan = d.IdLopHocPhan,
+                IdMon = mon.IdMonHoc,
+
+                DiemQuaTrinh = d.DiemQuaTrinh,
+                DiemKetThuc = d.DiemKetThuc,
+                DiemTongKet = d.DiemTongKet,
+                LanHoc = d.LanHoc,
+                TenMonHoc = mon.TenMonHoc,
+                TenLopHocPhan = lhp.TenHocPhan,
+            }
+        ).FirstOrDefault();
+
+        return new ApiResponse<DiemDto>
+        {
+            Status = true,
+            Message = "Success",
+            StatusCode = 200,
+            Data = query
+        };
+    }
 
     /**
      * Lay diem by id sinh vien
