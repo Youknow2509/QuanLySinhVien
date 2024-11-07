73bcdeb6d515516aaac1469b330d1b31166e065a
V <lytranvinh.work@gmail.com>
2024-11-08 00:38:31 +0700

    [upgeade]: them bien vao DiemDto

--- QLDT_WPF/Dto/DiemDto.cs
+++ QLDT_WPF/Dto/DiemDto.cs
@@ -5,9 +5,13 @@ namespace QLDT_WPF.Dto
         public string? IdDiem { get; set; }
         public string IdLopHocPhan { get; set; }
         public string IdSinhVien { get; set; }
+        public string? IdMon { get; set; }
+        
         public decimal DiemQuaTrinh { get; set; }
         public decimal DiemKetThuc { get; set; }
         public decimal DiemTongKet { get; set; }
         public int LanHoc { get; set; }
+        public string? TenMonHoc { get; set; }
+        public string? TenLopHocPhan { get; set; }
     }
 }
