0c6defb252eac20306e30d2b019e69cb2bee3c41
V <lytranvinh.work@gmail.com>
2024-11-08 00:47:32 +0700

    [upgeade]: lấy danh sách điểm có thể đăng kí nguyện vọng của sinh viên

--- QLDT_WPF/Repositories/DiemRepository.cs
+++ QLDT_WPF/Repositories/DiemRepository.cs
@@ -139,7 +139,7 @@ public class DiemRepository
     /**
      * Sua thong tin diem
      */
-
+    
 
     /**
      * Them diem
@@ -154,6 +154,59 @@ public class DiemRepository
      * Trả Về Danh Sách Điểm Của Sinh Viên Với Lần Thi Cuối Cùng, 
      * Có Điểm Tổng Kết >= 7, Chưa Có Trong Nguyện Vọng
      */
+    public async Task<ApiResponse<List<DiemDto>>> GetDiemDangKyNguyenVongFromSinhVien(string idSinhVien)
+    {
+        // Lấy các môn học có điểm tổng kết >= 7 và lần học là lớn nhất
+        var query = await (
+            from diem in _context.Diems
+            join lopHocPhan in _context.LopHocPhans
+                on diem.IdLopHocPhan equals lopHocPhan.IdLopHocPhan
+            join monhoc in _context.MonHocs
+                on lopHocPhan.IdMonHoc equals monhoc.IdMonHoc
+            where diem.IdSinhVien == idSinhVien && diem.DiemTongKet <= 7
+            join latestDiem in (
+                from d in _context.Diems
+                where d.IdSinhVien == idSinhVien && d.DiemTongKet <= 7
+                group d by d.IdLopHocPhan into g
+                select new
+                {
+                    IdLopHocPhan = g.Key,
+                    MaxLanHoc = g.Max(x => x.LanHoc)
+                }
+            ) on new { diem.IdLopHocPhan, diem.LanHoc } equals new { latestDiem.IdLopHocPhan, LanHoc = latestDiem.MaxLanHoc }
+            select new DiemDto
+            {
+                IdDiem = diem.IdDiem,
+                IdSinhVien = diem.IdSinhVien,
+                IdLopHocPhan = diem.IdLopHocPhan,
+                IdMon = monhoc.IdMonHoc,
+                TenLopHocPhan = lopHocPhan.TenHocPhan,
+                DiemQuaTrinh = diem.DiemQuaTrinh,
+                DiemKetThuc = diem.DiemKetThuc,
+                DiemTongKet = diem.DiemTongKet,
+                LanHoc = diem.LanHoc,
+                TenMonHoc = monhoc.TenMonHoc,
+            }
+        ).ToListAsync();
+
+        // lay cac mon hoc da dang ky cua sinh vien
+        var queryDangKy = await (
+            from dk in _context.DangKyNguyenVongs
+            where dk.IdSinhVien == idSinhVien
+            select dk.IdMonHoc
+        ).ToListAsync();
+
+        // Loại bỏ các môn học đã đăng ký
+        query = query.Where(x => !queryDangKy.Contains(x.IdMonHoc)).ToList();
+
+        return new ApiResponse<List<DiemDto>>
+        {
+            Status = true,
+            Message = "Success",
+            StatusCode = 200,
+            Data = query
+        };
+    }
 
     /**
      * Lay diem by id lop hoc phan
