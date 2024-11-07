542a4ef744273e4c00a0f6137c0f07da00097525
V <lytranvinh.work@gmail.com>
2024-11-07 17:28:50 +0700

    [upgrade]: todo cmp handle db khoa

---
+++ QLDT_WPF/Repositories/KhoaRepository.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+
+//
+using QLDT_WPF.Data;
+
+namespace QLDT_WPF.Repositories;
+
+public class KhoaRepository
+{
+    // Variables
+
+    // Constructor
+
+    /**
+     * Lay tat ca khoa
+     */
+
+
+    /**
+     * Lay khoa by id
+     */
+
+    /**
+     * Lay khoa by id
+     */
+
+
+    /**
+     * Sua thong tin khoa
+     */
+
+
+    /**
+     * Them khoa
+     */
+
+
+    /**
+     * Xoa khoa By Id 
+     */
+
+    /**
+     * Lay sinh vien thuoc khoa
+     */
+
+    /**
+     * Lay giao vien thuoc khoa
+     */
+    
+}
