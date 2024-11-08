6bd88cf26857ad1ef47e6c63aae392bb2c00fab3
V <lytranvinh.work@gmail.com>
2024-11-08 15:27:05 +0700

    [upgrade]: get khoa by id

--- QLDT_WPF/Repositories/KhoaRepository.cs
+++ QLDT_WPF/Repositories/KhoaRepository.cs
@@ -32,7 +32,7 @@ public class KhoaRepository
     /**
      * Lay tat ca khoa
      */
-    public async Task<ApiResponse<KhoaDto>> GetAll()
+    public async Task<ApiResponse<List<KhoaDto>>> GetAll()
     {
         var qr = await (
             from k in _context.Khoas
@@ -67,6 +67,48 @@ public class KhoaRepository
     /**
      * Lay khoa by id
      */
+    public async Task<ApiResponse<List<KhoaDto>>> GetById(string id)
+    {
+        var qr = await (
+            from k in _context.Khoas
+            where k.IdKhoa == id
+            join gv in _context.GiaoViens 
+                on k.IdGiaoVien equals gv.IdGiaoVien
+            join mh in _context.MonHocs
+                on k.IdMonHoc equals mh.IdMonHoc
+            join sv in _context.SinhViens
+                on k.IdSinhVien equals sv.IdSinhVien
+            select new KhoaDto
+            {
+                IdKhoa = k.IdKhoa,
+                IdSinhVien = sv.IdSinhVien,
+                IdGiaoVien = gv.IdGiaoVien, 
+                IdMonHoc = mh.IdMonHoc,
+
+                TenKhoa = k.TenKhoa,
+                TenSinhVien = sv.TenSinhVien, 
+                TenGiaoVien = gv.TenGiaoVien,
+                TenMonHoc = mh.TenMonHoc,
+            }
+        ).ToListAsync();
+
+        if (qr == null)
+        {
+            return new ApiResponse<KhoaDto>
+            {
+                Data = null,
+                Success = false,
+                Message = "Không tìm thấy dữ liệu"
+            };
+        }
+
+        return new ApiResponse<KhoaDto>
+        {
+            Data = qr,
+            Success = true,
+            Message = "Lấy dữ liệu thành công"
+        };
+    }
 
     /**
      * Lay khoa by id
