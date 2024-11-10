260bed9fb6ddcb16e0d58fdd1c7ff099805b24ad
V <lytranvinh.work@gmail.com>
2024-11-11 00:25:47 +0700

    [upgrade]: get all nguyen vong thay doi lich

--- QLDT_WPF/Repositories/NguyenVongGiaoVienRepository.cs
+++ QLDT_WPF/Repositories/NguyenVongGiaoVienRepository.cs
@@ -35,7 +35,37 @@ public class NguyenVongGiaoVienRepository
     /**
      * Lay tat ca nguyen vong cua giao vien
      */
+    public async Task<ApiResponse<List<NguyenVongThayDoiLichDto>>> GetAll()
+    {
+        var list_nguyen_vong = await (
+            from nguyenvong in _context.DangKyDoiLichs
+            join tg_lhp in _context.ThoiGianLopHocPhans
+                on nguyenvong.IdThoiGian equals tg_lhp.IdThoiGian
+            join lhp in _context.LopHocPhans
+                on tg_lhp.IdLopHocPhan equals lhp.IdLopHocPhan
+            select new NguyenVongThayDoiLichDto
+            {
+                IdDangKyDoiLich = nguyenvong.IdDangKyDoiLich,
+                IdThoiGian = nguyenvong.IdThoiGian,
+                IdLopHocPhan = lhp.IdLopHocPhan,
 
+                ThoiGianBatDauHienTai = nguyenvong.ThoiGianBatDauHienTai,
+                ThoiGianKetThucHienTai = nguyenvong.ThoiGianKetThucHienTai,
+                ThoiGianBatDauMoi = nguyenvong.ThoiGianBatDauMoi,
+                ThoiGianKetThucMoi = nguyenvong.ThoiGianKetThucMoi,
+
+                TrangThai = nguyenvong.TrangThai,
+                TenLopHocPhan = lhp.TenHocPhan,
+            }
+            ).ToListAsync();
+
+        return new ApiResponse<List<NguyenVongThayDoiLichDto>>
+        {
+            Data = list_nguyen_vong,
+            Success = true,
+            Message = "Lấy danh sách nguyện vọng thành công"
+        };
+    }
 
     /**
      * Lay nguyen vong cua giao vien by id
@@ -71,5 +101,5 @@ public class NguyenVongGiaoVienRepository
     /**
      * Tu choi nguyen vong by id
      */
-    
+
 }
