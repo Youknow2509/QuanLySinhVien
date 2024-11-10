b439128ef681ed51ac69f0ff44b523c1bf15df00
V <lytranvinh.work@gmail.com>
2024-11-11 01:53:05 +0700

    [upgrade]: get all sinh vien

--- QLDT_WPF/Repositories/SinhVienRepository.cs
+++ QLDT_WPF/Repositories/SinhVienRepository.cs
@@ -48,7 +48,30 @@ public class SinhVienRepository
     /**
      * Lay tat ca sinh vien
      */
-
+    public async Task<ApiResponse<List<SinhVienDto>>> GetAll()
+    {
+        var sinhViens = (
+            from sv in _context.SinhViens
+            join khoa in _context.Khoas 
+                on sv.IdKhoa equals khoa.IdKhoa
+            join cch in _context.ChuongTrinhHocs 
+                on sv.IdChuongTrinhHoc equals cch.IdChuongTrinhHoc
+            select new SinhVienDto
+            {
+                // List id
+                IdSinhVien = sv.IdSinhVien,
+                IdKhoa = sv.IdKhoa,
+                IdChuongTrinhHoc = sv.IdChuongTrinhHoc,
+                // list value
+                HoTen = sv.HoTen,
+                Lop = sv.Lop,
+                NgaySinh = sv.NgaySinh.HasValue ? sv.NgaySinh.Value.ToString("dd/MM/yyyy") : null,
+                DiaChi = sv.DiaChi,
+                TenKhoa = khoa.TenKhoa,
+                TenChuongTrinhHoc = cch.TenChuongTrinhHoc,
+            }
+        ).ToList();
+    }
 
     /**
      * Lay sinh vien by id
