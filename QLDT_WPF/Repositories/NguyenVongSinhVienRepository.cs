70cebcd0f22aa3e2cb6a0c3eef6787c7194feb3e
V <lytranvinh.work@gmail.com>
2024-11-11 01:13:35 +0700

    [upgrade]: init nguyen vong sinh vien

--- QLDT_WPF/Repositories/NguyenVongSinhVienRepository.cs
+++ QLDT_WPF/Repositories/NguyenVongSinhVienRepository.cs
@@ -1,16 +1,36 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 //
 using QLDT_WPF.Data;
+using QLDT_WPF.Dto;
+using QLDT_WPF.Models;
 
 namespace QLDT_WPF.Repositories;
 
 public class NguyenVongSinhVienRepository
 {
     // Variables
+    private readonly QuanLySinhVienDbContext _context;
 
     // Constructor
+    public NguyenVongSinhVienRepository()
+    {
+        // Connect to database QuanLySinhVienDbContext
+        var connectionString = ConfigurationManager.ConnectionStrings["QuanLySinhVienDbConnection"].ConnectionString;
+        _context = new QuanLySinhVienDbContext(
+            new DbContextOptionsBuilder<QuanLySinhVienDbContext>()
+                .UseSqlServer(connectionString)
+                .Options);
+    }
+
+    // Dispose
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
 
     /**
      * Lay tat ca nguyen vong cua sinh vien
@@ -51,5 +71,5 @@ public class NguyenVongSinhVienRepository
     /**
      * Tu choi nguyen vong by id
      */
-    
+
 }
