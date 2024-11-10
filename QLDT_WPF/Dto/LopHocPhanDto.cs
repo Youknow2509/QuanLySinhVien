33d624bba3edd65b3fd7bb11621e6888f16a233f
V <lytranvinh.work@gmail.com>
2024-11-10 21:05:10 +0700

    [fix] : fix ci cd 7c88d79

--- QLDT_WPF/Dto/LopHocPhanDto.cs
+++ QLDT_WPF/Dto/LopHocPhanDto.cs
@@ -3,11 +3,11 @@ namespace QLDT_WPF.Dto
     public class LopHocPhanDto
     {
         public string? IdLopHocPhan { get; set; }
-        public string TenLopHocPhan { get; set; }
-        public string IdGiaoVien { set; get; }
-        public string IdMonHoc { set; get; }
-        public DateTime ThoiGianBatDau { set; get; }
-        public DateTime ThoiGianKetThuc { set; get; }
+        public string? TenLopHocPhan { get; set; }
+        public string? IdGiaoVien { set; get; }
+        public string? IdMonHoc { set; get; }
+        public DateTime? ThoiGianBatDau { set; get; }
+        public DateTime? ThoiGianKetThuc { set; get; }
 
         public string? TenGiaoVien { get; set; }
         public string? TenMonHoc { get; set; }
