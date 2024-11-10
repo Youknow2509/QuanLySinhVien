b869d74bb95d1059421b8e2c5e4fd5ae9c857135
V <lytranvinh.work@gmail.com>
2024-11-10 23:36:44 +0700

    [upgrade]: init mon hoc repostitory

--- QLDT_WPF/Repositories/MonHocRepository.cs
+++ QLDT_WPF/Repositories/MonHocRepository.cs
@@ -9,9 +9,25 @@ namespace QLDT_WPF.Repositories;
 public class MonHocRepository
 {
     // Variables
+    private readonly QuanLySinhVienDbContext _context;
 
     // Constructor
+    public MonHocRepository()
+    {
+        // Connect to database QuanLySinhVienDbContext
+        var connectionString = ConfigurationManager.ConnectionStrings["QuanLySinhVienDbConnection"].ConnectionString;
+        _context = new QuanLySinhVienDbContext(
+            new DbContextOptionsBuilder<QuanLySinhVienDbContext>()
+                .UseSqlServer(connectionString)
+                .Options);
+    }
 
+    // Dispose
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+    
     /**
      * Lay tat ca mon hoc
      */
@@ -43,5 +59,5 @@ public class MonHocRepository
     /**
      * Get data mon hoc for giao vien with id giao vien
      */
-    
+
 }
