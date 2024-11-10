5725e20770645ea75f06ef88b77bb3af000a7f28
V <lytranvinh.work@gmail.com>
2024-11-11 01:17:34 +0700

    [upgrade]: handle get all nguyen vong sinh vien

--- QLDT_WPF/Dto/NguyenVongSinhVienDto.cs
+++ QLDT_WPF/Dto/NguyenVongSinhVienDto.cs
@@ -3,8 +3,11 @@ namespace QLDT_WPF.Dto
     public class NguyenVongSinhVienDto
     {
         public string? IdNguyenVong { get; set; }
-        public string IdSinhVien { get; set; }
-        public string IdMonHoc { get; set; }
+        public string? IdSinhVien { get; set; }
+        public string? IdMonHoc { get; set; }
+
+        public string? TenSinhVien { get; set; }
+        public string? TenMonHoc { get; set; }
         public int? TrangThai { get; set; }
     }
 }
