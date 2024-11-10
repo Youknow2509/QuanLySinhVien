ecfa9c3581308ba2bae56873bdc347b60f915670
V <lytranvinh.work@gmail.com>
2024-11-11 00:25:13 +0700

    [upgrade]: upgrade nguyen vong thay doi lich dto

--- QLDT_WPF/Dto/NguyenVongThayDoiLichDto.cs
+++ QLDT_WPF/Dto/NguyenVongThayDoiLichDto.cs
@@ -5,11 +5,15 @@ namespace QLDT_WPF.Dto
     public class NguyenVongThayDoiLichDto
     {
         // Variables
-        public string? IdDangKyDoiLich { get; set; } = null!;
-        public string IdThoiGian { get; set; } = null!;
-        public DateTime ThoiGianBatDauHienTai { get; set; }
-        public DateTime ThoiGianKetThucHienTai { get; set; }
-        public DateTime ThoiGianBatDauMoi { get; set; }
-        public DateTime ThoiGianKetThucMoi { get; set; }
+        public string? IdDangKyDoiLich { get; set; }
+        public string? IdThoiGian { get; set; }
+        public string? IdLopHocPhan { get; set; }
+
+        public DateTime? ThoiGianBatDauHienTai { get; set; }
+        public DateTime? ThoiGianKetThucHienTai { get; set; }
+        public DateTime? ThoiGianBatDauMoi { get; set; }
+        public DateTime? ThoiGianKetThucMoi { get; set; }
+        public int? TrangThai { get; set; } 
+        public string? TenLopHocPhan { get; set; }
     }
 }
