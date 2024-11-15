bca2a6e875040a64f98e40f96c66e41dd152daf8
V <lytranvinh.work@gmail.com>
2024-11-16 00:31:43 +0700

    [upgrade]: handle render view mon hoc from admin

--- QLDT_WPF/Views/Components/SubjectsTableView.xaml.cs
+++ QLDT_WPF/Views/Components/SubjectsTableView.xaml.cs
@@ -6,6 +6,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using QLDT_WPF.Dto;
+using QLDT_WPF.Repositories;
 
 namespace QLDT_WPF.Views.Components
 {
@@ -14,10 +18,38 @@ namespace QLDT_WPF.Views.Components
     /// </summary>
     public partial class SubjectsTableView : UserControl
     {
+        // Variables
+        private MonHocRepository monHocRepository;
+        public ObservableCollection<MonHocDto> ObservableMonHoc { get; private set; }
+
+        // Constructor
         public SubjectsTableView()
         {
             InitializeComponent();
+            monHocRepository = new MonHocRepository();
+            ObservableMonHoc = new ObservableCollection<MonHocDto>();
+            // Handle loading asynchronously
+            Loaded += async (sender, e) =>
+            {
+                await InitAsync();
+            };
+        }
 
+        // Init window asynchronously
+        private async Task InitAsync()
+        {
+            var list = await monHocRepository.GetAll();
+            if (list.Status == false)
+            {
+                MessageBox.Show(list.Message);
+                return;
+            }
+            ObservableMonHoc.Clear();
+            foreach (var item in list.Data)
+            {
+                ObservableMonHoc.Add(item);
+            }
+            dataGridMonHoc.ItemsSource = ObservableMonHoc;
         }
     }
 }
