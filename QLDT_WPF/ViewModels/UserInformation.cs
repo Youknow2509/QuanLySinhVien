853ea3af356afb0e3ddcfb5fcd1d646a251aa41b
V <lytranvinh.work@gmail.com>
2024-11-15 11:43:12 +0700

    [upgrade]: create UserInformation init save in window

---
+++ QLDT_WPF/ViewModels/UserInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLDT_WPF.ViewModels
+{
+    public class UserInformation
+    {
+        public string? IdUser { get; set; }
+        public string? IdClaim { get; set; }
+        public string? UserName { get; set; }
+        public string? RoleName { get; set; }
+        public string? FullName { get; set; }
+        public byte[]? Image { get; set; }
+    }
+}
