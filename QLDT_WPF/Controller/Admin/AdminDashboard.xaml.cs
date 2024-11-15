e26534c3cb921ca71bb5bbe8fa5a1dfecd5272af
V <lytranvinh.work@gmail.com>
2024-11-15 11:44:46 +0700

    [upgrade]: xử lí truyền dữ liệu vào trong component

--- QLDT_WPF/Controller/Admin/AdminDashboard.xaml.cs
+++ QLDT_WPF/Controller/Admin/AdminDashboard.xaml.cs
@@ -1,14 +1,71 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
+using QLDT_WPF.Repositories;
+using QLDT_WPF.ViewModels;
 
 namespace QLDT_WPF.Views.Admin
 {
-    public partial class AdminDashboard : Window
+    public partial class AdminDashboard : Window, INotifyPropertyChanged
     {
-        public AdminDashboard()
+        // Variables
+        private readonly UserInformation userInformation;
+
+        private ChuongTrinhHocRepository chuongTrinhHocRepository;
+        private DiemRepository diemRepository;
+        private GiaoVienRepository giaoVienRepository;
+        private IdentityRepository identityRepository;
+        private KhoaRepository khoaRepository;
+        private LopHocPhanRepository lopHocPhanRepository;
+        private MonHocRepository monHocRepository;
+        private NguyenVongGiaoVienRepository nguyenVongGiaoVienRepository;
+        private NguyenVongSinhVienRepository NguyenVongSinhVienRepository;
+        private SinhVienRepository sinhVienRepository;
+
+        // Data Context
+        private string _fullName;
+        public string FullName
+        {
+            get => _fullName;
+            set
+            {
+                _fullName = value;
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        // kích hoạt bất cứ khi nào một thuộc tính trong lớp thay đổi ~ INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // Constructor
+        public AdminDashboard(UserInformation userInformation)
         {
             InitializeComponent();
+            this.userInformation = userInformation;
+            chuongTrinhHocRepository = new ChuongTrinhHocRepository();
+            diemRepository = new DiemRepository();
+            giaoVienRepository = new GiaoVienRepository();
+            identityRepository = new IdentityRepository();
+            khoaRepository = new KhoaRepository();
+            lopHocPhanRepository = new LopHocPhanRepository();
+            monHocRepository = new MonHocRepository();
+            nguyenVongGiaoVienRepository = new NguyenVongGiaoVienRepository();
+            NguyenVongSinhVienRepository = new NguyenVongSinhVienRepository();
+            sinhVienRepository = new SinhVienRepository();
+            //
+            DataContext = this;
+            // init
+            this.initWindow();
+        }
+
+        // Handle event when window loaded
+        private void initWindow()
+        {
+            this.FullName = userInformation.FullName;
         }
     }
 }
--- QLDT_WPF/Views/Shared/Header.xaml.cs
+++ QLDT_WPF/Views/Shared/Header.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using QLDT_WPF.Views.Admin;
+using QLDT_WPF.Views.Shared;
+using QLDT_WPF.Views.Login;
 
 namespace QLDT_WPF.Views.Shared
 {
@@ -19,9 +22,39 @@ namespace QLDT_WPF.Views.Shared
     /// </summary>
     public partial class Header : UserControl
     {
+        // variables
+
+        public string FullName
+        {
+            get => (string)GetValue(FullNameProperty);
+            set => SetValue(FullNameProperty, value);
+        }
+
+        // DependencyProperty
+        public static readonly DependencyProperty FullNameProperty =
+             DependencyProperty.Register("FullName", 
+                 typeof(string), 
+                 typeof(Header),
+                 new PropertyMetadata(string.Empty));
+     
+
+        // Constructor
         public Header()
         {
             InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Bạn có chắc chắn muốn đăng xuất?", "Đăng xuất", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                // Changle to windown login
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.Show();
+                Window.GetWindow(this).Close();
+
+            }
         }
     }
 }
