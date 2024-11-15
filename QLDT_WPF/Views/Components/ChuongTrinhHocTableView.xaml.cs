6fecccbfea299d4cfd8de15ee3330ee9fd3590c6
hoangmanhkhiem <khiemhm04@gmail.com>
2024-11-15 23:03:35 +0700

    update chuongtrionhhoctableview

--- QLDT_WPF/Views/Components/ChuongTrinhHocTableView.xaml.cs
+++ QLDT_WPF/Views/Components/ChuongTrinhHocTableView.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+// Your namespaces
+using QLDT_WPF.Repositories;
+using QLDT_WPF.Dto;
 
 namespace QLDT_WPF.Views.Components
 {
@@ -20,9 +15,41 @@ namespace QLDT_WPF.Views.Components
     /// </summary>
     public partial class ChuongTrinhHocTableView : UserControl
     {
+        // Variables
+        private ChuongTrinhHocRepository chuongTrinhHocRepository;
+        public ObservableCollection<ChuongTrinhHocDto> ObservableChuongTrinhHoc { get; private set; }
+
+        // Constructor
         public ChuongTrinhHocTableView()
         {
             InitializeComponent();
+            chuongTrinhHocRepository = new ChuongTrinhHocRepository();
+            ObservableChuongTrinhHoc = new ObservableCollection<ChuongTrinhHocDto>();
+
+            // Hook up Loaded event to call async initialization after control loads
+            Loaded += async (s, e) => await InitAsync();
+        }
+
+        // Init window asynchronously
+        private async Task InitAsync()
+        {
+            var list_chuongTrinhHoc = await chuongTrinhHocRepository.GetAll();
+
+            // Handle unsuccessful response
+            if (list_chuongTrinhHoc.Status == false)
+            {
+                MessageBox.Show(list_chuongTrinhHoc.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Add items to ObservableCollection
+            foreach (var item in list_chuongTrinhHoc.Data)
+            {
+                ObservableChuongTrinhHoc.Add(item);
+            }
+
+            // Bind to DataGrid or other UI components as needed
+            dataGridPrograms.ItemsSource = ObservableChuongTrinhHoc;
         }
     }
 }
