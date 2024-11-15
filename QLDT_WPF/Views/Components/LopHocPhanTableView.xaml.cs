d631a0722abe5ce99ecc8d7d44ebf5ed74b930b3
V <lytranvinh.work@gmail.com>
2024-11-16 00:07:35 +0700

    [upgrade]: handle render view lop hoc phan in admin

--- QLDT_WPF/Views/Components/LopHocPhanTableView.xaml.cs
+++ QLDT_WPF/Views/Components/LopHocPhanTableView.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using QLDT_WPF.Dto;
+using QLDT_WPF.Repositories;
 
 namespace QLDT_WPF.Views.Components
 {
@@ -19,9 +21,40 @@ namespace QLDT_WPF.Views.Components
     /// </summary>
     public partial class LopHocPhanTableView : UserControl
     {
+        // Variables
+        private LopHocPhanRepository lopHocPhanRepository;
+        public ObservableCollection<LopHocPhanDto> ObservableKhoa { get; private set; }
+
+        // Constructor
         public LopHocPhanTableView()
         {
             InitializeComponent();
+            lopHocPhanRepository = new LopHocPhanRepository();
+            ObservableKhoa = new ObservableCollection<LopHocPhanDto>();
+            // Load asynchronously
+            Loaded += async (s, e) => await InitAsync();
+        }
+
+        // Init window asynchronously
+        private async Task InitAsync()
+        {
+            var list_lopHocPhan = await lopHocPhanRepository.GetAll();
+
+            // Handle unsuccessful response
+            if (list_lopHocPhan.Status == false)
+            {
+                MessageBox.Show(list_lopHocPhan.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Add items to ObservableCollection
+            foreach (var item in list_lopHocPhan.Data)
+            {
+                ObservableKhoa.Add(item);
+            }
+
+            // Bind to DataGrid or other UI components as needed
+            dataGridLopHocPhan.ItemsSource = ObservableKhoa;
         }
     }
 }
