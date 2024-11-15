bb5f695499c0e3bd35e4e7d60667bb83c6b2ad70
V <lytranvinh.work@gmail.com>
2024-11-16 00:37:11 +0700

    [upgrade]: handle render view giao vien for admin

--- QLDT_WPF/Views/Components/TeacherTableView.xaml.cs
+++ QLDT_WPF/Views/Components/TeacherTableView.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using QLDT_WPF.Dto;
+using QLDT_WPF.Repositories;
 
 namespace QLDT_WPF.Views.Components
 {
@@ -19,9 +21,38 @@ namespace QLDT_WPF.Views.Components
     /// </summary>
     public partial class TeacherTableView : UserControl
     {
+        // Variables
+        private GiaoVienRepository giaoVienRepository;
+        public ObservableCollection<GiaoVienDto> ObservableGiaoVien { get; private set; }
+
+        // Constructor
         public TeacherTableView()
         {
             InitializeComponent();
+            giaoVienRepository = new GiaoVienRepository();
+            ObservableGiaoVien = new ObservableCollection<GiaoVienDto>();
+            // Handle loading asynchronously
+            Loaded += async (sender, e) =>
+            {
+                await InitAsync();
+            };
+        }
+
+        // Init window asynchronously
+        private async Task InitAsync()
+        {
+            var list = await giaoVienRepository.GetAll();
+            if (list.Status == false)
+            {
+                MessageBox.Show(list.Message);
+                return;
+            }
+            ObservableGiaoVien.Clear();
+            foreach (var item in list.Data)
+            {
+                ObservableGiaoVien.Add(item);
+            }
+            dataGridGiaoVien.ItemsSource = ObservableGiaoVien;
         }
     }
 }
