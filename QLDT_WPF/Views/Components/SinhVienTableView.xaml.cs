b0500d1b7686f161993c9ef3acbc34156ea2d150
Khiem <khiemhm04@gmail.com>
2024-11-16 12:21:30 +0700

    fix 1 sinhvientbl

--- QLDT_WPF/Views/Components/SinhVienTableView.xaml.cs
+++ QLDT_WPF/Views/Components/SinhVienTableView.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using QLDT_WPF.Dto;
 using QLDT_WPF.Repositories;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Collections.ObjectModel;
 
 namespace QLDT_WPF.Views.Components
 {
@@ -26,6 +16,7 @@ namespace QLDT_WPF.Views.Components
         // Variables
         private SinhVienRepository sinhVienRepository;
         public ObservableCollection<SinhVienDto> ObservableSinhVien { get; private set; }
+        public ObservableCollection<SinhVienDto> FilteredSinhVien { get; private set; }
 
         // Constructor
         public SinhVienTableView()
@@ -33,12 +24,19 @@ namespace QLDT_WPF.Views.Components
             InitializeComponent();
             sinhVienRepository = new SinhVienRepository();
             ObservableSinhVien = new ObservableCollection<SinhVienDto>();
+            FilteredSinhVien = new ObservableCollection<SinhVienDto>();
+
             // Handle loading asynchronously
             Loaded += async (sender, e) =>
             {
                 await InitAsync();
             };
+
+            // Initialize events
+            txtTimKiem.TextChanged += SearchTextChanged;
+            cbbPageSize.SelectionChanged += PageSizeChanged;
         }
+
         // Init window asynchronously
         private async Task InitAsync()
         {
@@ -48,12 +46,45 @@ namespace QLDT_WPF.Views.Components
                 MessageBox.Show(list.Message);
                 return;
             }
+
             ObservableSinhVien.Clear();
             foreach (var item in list.Data)
             {
                 ObservableSinhVien.Add(item);
             }
-            dataGridStudents.ItemsSource = ObservableSinhVien;
+
+            // Initialize filtered collection
+            FilteredSinhVien = new ObservableCollection<SinhVienDto>(ObservableSinhVien);
+
+            // Bind data to SfDataPager
+            sfDataPager.Source = FilteredSinhVien;
+            sfDataGrid.ItemsSource = sfDataPager.PagedSource;
+        }
+
+        // Handle search functionality
+        private void SearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchText = txtTimKiem.Text.ToLower();
+
+            // Filter data based on search text
+            var filteredData = ObservableSinhVien.Where(s =>
+                s.HoTen.ToLower().Contains(searchText) ||
+                s.DiaChi.ToLower().Contains(searchText) ||
+                s.TenChuongTrinhHoc.ToLower().Contains(searchText)).ToList();
+
+            FilteredSinhVien = new ObservableCollection<SinhVienDto>(filteredData);
+
+            // Refresh pager source
+            sfDataPager.Source = FilteredSinhVien;
+        }
+
+        // Handle page size change
+        private void PageSizeChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbbPageSize.SelectedItem is ComboBoxItem selectedItem)
+            {
+                sfDataPager.PageSize = int.Parse(selectedItem.Content.ToString());
+            }
         }
     }
 }
