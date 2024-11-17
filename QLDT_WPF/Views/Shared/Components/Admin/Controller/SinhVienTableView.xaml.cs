322a9c12352b12589e5fcccbe01f1518cb989697
V <lytranvinh.work@gmail.com>
2024-11-18 01:37:49 +0700

    [fix]: fix add package using

--- QLDT_WPF/Views/Shared/Components/Admin/Controller/SinhVienTableView.xaml.cs
+++ QLDT_WPF/Views/Shared/Components/Admin/Controller/SinhVienTableView.xaml.cs
@@ -1,10 +1,28 @@
-﻿using QLDT_WPF.Dto;
+﻿using Syncfusion.UI.Xaml.Grid.Converter;
+using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.XlsIO;
+using QLDT_WPF.Dto;
 using QLDT_WPF.Repositories;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using QLDT_WPF.Models;
+using Microsoft.Win32;
+using System.IO;
+using System;
 
 namespace QLDT_WPF.Views.Components
 {
