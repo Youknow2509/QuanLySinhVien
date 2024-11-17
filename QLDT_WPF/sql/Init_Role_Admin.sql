30860ffa4aa082e7a50b2f5575b9210d302a3a5b
hoangmanhkhiem <khiemhm04@gmail.com>
2024-11-17 11:44:56 +0700

    add sql init user role

---
+++ QLDT_WPF/sql/Init_Role_Admin.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO Roles (Id,Name, NormalizedName)
+    VALUES
+        ('1','Admin', 'ADMIN'),
+        ('2','SinhVien', 'SINHVIEN'),
+        ('3','GiaoVien', 'GIAOVIEN');
+
+INSERT INTO Users (Id,IdClaim, FullName,Phone,Email,PasswordHash, UserName,EmailConfirmed, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled, AccessFailedCount)
+VALUES
+    (NEWID(),'admin', 'Admin', 1, NULL, 'i1CelkDpmAmgU08yFCskzfda4mWOI12kwgW571+2OiY=', 'admin', 1, 1, 1, 1, 1);
+
+--Gan quyen cho admin
+INSERT INTO UserRoles (UserId,RoleId)
+VALUES
+    ((SELECT Id FROM Users WHERE UserName = 'admin'), (SELECT Id FROM Roles WHERE Name = 'Admin'));
