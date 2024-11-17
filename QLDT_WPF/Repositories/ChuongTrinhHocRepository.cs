9c4689a4d9f2793ed4a86574144add5b73d1a00c
V <lytranvinh.work@gmail.com>
2024-11-17 23:23:31 +0700

    [upgrade]: handle api add list chuong trinh hoc

--- QLDT_WPF/Repositories/ChuongTrinhHocRepository.cs
+++ QLDT_WPF/Repositories/ChuongTrinhHocRepository.cs
@@ -162,6 +162,83 @@ namespace QLDT_WPF.Repositories
         }
 
         /**
+         * Thêm danh sách chương trình học từ file CSV
+         */
+        public async Task<ApiResponse<List<ChuongTrinhHocDto>>> 
+            AddListChuongTrinhHocFromCSV(List<ChuongTrinhHocDto> listChuongTrinhHoc)
+        {
+            // Kiểm tra nếu danh sách null hoặc trống
+            if (listChuongTrinhHoc == null || !listChuongTrinhHoc.Any())
+            {
+                return new ApiResponse<List<ChuongTrinhHocDto>>
+                {
+                    Status = false,
+                    Message = "File Không Được Để Trống!",
+                    Data = null,
+                };
+            }
+
+            List<ChuongTrinhHocDto> listChuongTrinhHocError = new List<ChuongTrinhHocDto>();
+            HashSet<string> processedIds = new HashSet<string>();
+
+            // Kiểm tra các bản ghi trùng lặp trong danh sách CSV
+            foreach (var monh in listChuongTrinhHoc)
+            {
+                if (processedIds.Contains(monh.IdMonHoc))
+                {
+                    monh.TenMonHoc = $"Chương Trình Học: {monh.TenMonHoc} lỗi trùng ID {monh.IdMonHoc} trong file CSV";
+                    listChuongTrinhHocError.Add(monh);
+                    continue;
+                }
+
+                processedIds.Add(monh.IdMonHoc);
+            }
+
+            // Loại bỏ các bản ghi trùng lặp khỏi danh sách trước khi kiểm tra với CSDL
+            var uniquelistChuongTrinhHoc = listChuongTrinhHoc.Except(listChuongTrinhHocError).ToList();
+
+            // Kiểm tra với cơ sở dữ liệu và thêm vào danh sách lỗi nếu cần
+            foreach (var cch in uniquelistChuongTrinhHoc)
+            {
+                var cct_c = await _context.ChuongTrinhHocs
+                    .FirstOrDefaultAsync(x => x.IdChuongTrinhHoc == cch.IdChuongTrinhHoc);
+                if (cct_c != null)
+                {
+                    cch.TenChuongTrinhHoc = $"Chương Trình Học: {cch.TenChuongTrinhHoc} đã tồn tại trong CSDL";
+                    listChuongTrinhHocError.Add(cch);
+                    continue;
+                }
+                // Nếu không có lỗi, thêm vào CSDL
+                await _context.ChuongTrinhHocs.AddAsync(new ChuongTrinhHocDto
+                {
+                    IdChuongTrinhHoc = cch.IdChuongTrinhHoc,
+                    TenChuongTrinhHoc = cch.TenChuongTrinhHoc
+                });
+            }
+
+            // Nếu có bất kỳ lỗi nào trong quá trình xử lý
+            if (listChuongTrinhHocError.Any())
+            {
+                return new ApiResponse<List<ChuongTrinhHocDto>>
+                {
+                    Status = false,
+                    Message = "Thêm Danh Sách Chương Trình Học Thất Bại! Có lỗi trong danh sách chương trình học.",
+                    Data = listMonHocError,
+                };
+            }
+
+            // Lưu thay đổi nếu mọi thứ thành công
+            await _context.SaveChangesAsync();
+
+            return new ApiResponse<List<ChuongTrinhHocDto>>
+            {
+                Status = true,
+                Message = "Thêm Danh Sách Chương Trình Học Thành Công!",
+                Data = listChuongTrinhHoc,
+            };
+        }
+
+        /**
          * Xóa chương trình học theo ID
          */
         public async Task<ApiResponse<bool>> Delete(string id)
@@ -195,13 +272,13 @@ namespace QLDT_WPF.Repositories
         /**
          * Lấy danh sách môn học có và không có trong chương trình học theo ID chương trình học
          */
-        public async Task<ApiResponse<(List<MonHocDto> MonHocInChuongTrinhHoc, List<MonHocDto> MonHocKhongCo)>> GetMonHocByIdChuongTrinhHoc(string id)
+        public async Task<ApiResponse<(List<ChuongTrinhHocDto> MonHocInChuongTrinhHoc, List<ChuongTrinhHocDto> MonHocKhongCo)>> GetMonHocByIdChuongTrinhHoc(string id)
         {
             var monHocInChuongTrinhHoc = await (
                 from cch_mh in _context.ChuongTrinhHocMonHocs
                 where cch_mh.IdChuongTrinhHoc == id
                 join mh in _context.MonHocs on cch_mh.IdMonHoc equals mh.IdMonHoc
-                select new MonHocDto
+                select new ChuongTrinhHocDto
                 {
                     IdMonHoc = mh.IdMonHoc,
                     TenMonHoc = mh.TenMonHoc,
@@ -218,7 +295,7 @@ namespace QLDT_WPF.Repositories
                     where cch_mh.IdChuongTrinhHoc == id
                     select cch_mh.IdMonHoc
                 ).Contains(mh.IdMonHoc)
-                select new MonHocDto
+                select new ChuongTrinhHocDto
                 {
                     IdMonHoc = mh.IdMonHoc,
                     TenMonHoc = mh.TenMonHoc,
@@ -228,7 +305,7 @@ namespace QLDT_WPF.Repositories
                 }
             ).ToListAsync();
 
-            return new ApiResponse<(List<MonHocDto>, List<MonHocDto>)>
+            return new ApiResponse<(List<ChuongTrinhHocDto>, List<ChuongTrinhHocDto>)>
             {
                 Status = true,
                 StatusCode = 200,
