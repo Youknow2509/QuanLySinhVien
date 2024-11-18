b5373906015271dfe14106a162685faa05ab4539
V <lytranvinh.work@gmail.com>
2024-11-18 13:39:51 +0700

    [upgrade]: handle add khoa in windown helper

--- QLDT_WPF/Views/Shared/Components/Admin/Help/Khoa/AddKhoa.cs
+++ QLDT_WPF/Views/Shared/Components/Admin/Help/Khoa/AddKhoa.cs
@@ -22,24 +22,14 @@ namespace QLDT_WPF.Views.Shared.Components.Admin.Help
     public partial class AddKhoa : Window
     {
         // Variables
-        
+        private KhoaRepository khoaRepository;
 
         // Constructor
         public AddKhoa()
         {
             InitializeComponent();
 
-            
-            // init select box khoa
-            Loaded += async (sender, e) =>
-            {
-                await InitAsync();
-            };
-        }
-
-        // Init window asynchronously
-        private async Task InitAsync()
-        {
+            khoaRepository = new KhoaRepository();
             
         }
 
@@ -50,13 +40,47 @@ namespace QLDT_WPF.Views.Shared.Components.Admin.Help
 
         private void CreateButton_Click(object sender, RoutedEventArgs e)
         {
-            // TODO 
-        }
+            string tenKhoa = txtTenKhoa.Text.Trim();
+            string maKhoa = txtMaKhoa.Text.Trim();
+            if (tenKhoa == "" || maKhoa == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
 
-        // Only allow integer input in text box
-        private void handle_input_key_press_number(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !int.TryParse(e.Text, out _);
+            KhoaDto newkhoaDto = new KhoaDto()
+            {
+                IdKhoa = maKhoa,
+                TenKhoa = tenKhoa
+            };
+
+            try
+            {
+                // Sử dụng Task.Run để chạy hàm bất đồng bộ và đợi kết quả
+                var response = Task.Run(async () => 
+                    await khoaRepository.Add(newkhoaDto)
+                ).Result;
+
+                // Kiểm tra kết quả trả về
+                if (response.Status == true)
+                {
+                    MessageBox.Show(response.Message, "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Close(); // Đóng cửa sổ nếu thêm thành công
+                }
+                else
+                {
+                    MessageBox.Show(response.Message, "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (AggregateException ex)
+            {
+                // Xử lý ngoại lệ bất đồng bộ
+                foreach (var innerEx in ex.InnerExceptions)
+                {
+                    MessageBox.Show($"Có lỗi xảy ra: {innerEx.Message}", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
         }
     }
 }
