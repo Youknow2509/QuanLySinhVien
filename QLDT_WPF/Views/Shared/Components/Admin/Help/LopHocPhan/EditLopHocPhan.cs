b6cf28051115e719cd527f98f8dfd6ae3528c784
V <lytranvinh.work@gmail.com>
2024-11-18 16:23:40 +0700

    [upgrade]: handle int edit lop hoc phan in windown helper

--- QLDT_WPF/Views/Shared/Components/Admin/Help/LopHocPhan/EditLopHocPhan.cs
+++ QLDT_WPF/Views/Shared/Components/Admin/Help/LopHocPhan/EditLopHocPhan.cs
@@ -22,11 +22,28 @@ namespace QLDT_WPF.Views.Shared.Components.Admin.Help
     public partial class EditLopHocPhan : Window
     {
         // Variable
+        private LopHocPhanDto lopHocPhan;
+
+        private LopHocPhanRepository lopHocPhanRepository;
+        private MonHocRepository monHocRepository;
+        private GiaoVienRepository giaoVienRepository;
+
+        private List<MonHocDto> list_mon_hoc;
+        private List<GiaoVienDto> list_giao_vien;
 
         // Constructor
-        public EditLopHocPhan(LopHocPhanDto lopHocPhanDto)
+        public EditLopHocPhan(LopHocPhanDto l)
         {
             InitializeComponent();
+
+            lopHocPhan = l;
+
+            lopHocPhanRepository = new LopHocPhanRepository();
+            monHocRepository = new MonHocRepository();
+            giaoVienRepository = new GiaoVienRepository();
+
+            list_mon_hoc = new List<MonHocDto>();
+            list_giao_vien = new List<GiaoVienDto>();
 
             // Handle loading asynchronously init
             Loaded += async (sender, e) =>
@@ -38,7 +55,47 @@ namespace QLDT_WPF.Views.Shared.Components.Admin.Help
         // Async init view
         private async Task InitAsync()
         {
-            // TODO
+            // Set values
+            txtIdLopHocPhan.Text = lopHocPhan.IdLopHocPhan;
+            txtTenLopHocPhan.Text = lopHocPhan.TenLopHocPhan;
+            dpThoiGianBatDau.SelectedDate = lopHocPhan.ThoiGianBatDau;
+            dpThoiGianKetThuc.SelectedDate = lopHocPhan.ThoiGianKetThuc;
+
+            // Load list mon hoc and set value for cbbMonHoc
+            var request_lis_mon_hoc = await monHocRepository.GetAll();
+            if (request_lis_mon_hoc.Status == false)
+            {
+                MessageBox.Show(request_lis_mon_hoc.Message);
+                return;
+            }
+            list_mon_hoc = request_lis_mon_hoc.Data;
+            foreach (var item in list_mon_hoc)
+            {
+                cbbMonHoc.Items.Add(new ComboBoxItem
+                {
+                    Content = item.TenMonHoc,
+                    Tag = item.IdMonHoc
+                });
+            }
+            cbbMonHoc.SelectedValue = lopHocPhan.IdMonHoc;
+
+            // Load list giao vien and set value for cbbGiaoVien
+            var request_list_giao_vien = await giaoVienRepository.GetAll();
+            if (request_list_giao_vien.Status == false)
+            {
+                MessageBox.Show(request_list_giao_vien.Message);
+                return;
+            }
+            list_giao_vien = request_list_giao_vien.Data;
+            foreach (var item in list_giao_vien)
+            {
+                cbbGiaoVien.Items.Add(new ComboBoxItem
+                {
+                    Content = item.TenGiaoVien,
+                    Tag = item.IdGiaoVien
+                });
+            }
+            cbbGiaoVien.SelectedValue = lopHocPhan.IdGiaoVien;
         }
 
         // Handle close button click
