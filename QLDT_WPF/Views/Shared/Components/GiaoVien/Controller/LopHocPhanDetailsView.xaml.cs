da0c1150816becc408b4237f26343c59b707ab3d
hoangmanhkhiem <khiemhm04@gmail.com>
2024-11-18 23:59:42 +0700

    add new lhpdetail

---
+++ QLDT_WPF/Views/Shared/Components/GiaoVien/Controller/LopHocPhanDetailsView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using QLDT_WPF.Views.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace QLDT_WPF.Views.Shared.Components.GiaoVien.View
+{
+    /// <summary>
+    /// Interaction logic for LopHocPhanDetailsView.xaml
+    /// </summary>
+    public partial class LopHocPhanDetailsView : UserControl
+    {
+        public ContentControl TargetContentArea { get; set; }
+        public LopHocPhanDetailsView(ContentControl targetContentArea)
+        {
+            InitializeComponent();
+            TargetContentArea = targetContentArea;
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TargetContentArea != null)
+            {
+                TargetContentArea.Content = new TeacherTableView
+                {
+                    TargetContentArea = TargetContentArea
+                };
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy khu vực hiển thị nội dung!", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}
