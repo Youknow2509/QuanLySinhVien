31e7a2adcec1be62177f06f6108896d7fc081ca3
hoangmanhkhiem <khiemhm04@gmail.com>
2024-11-18 23:59:54 +0700

    add new lhp tbl

---
+++ QLDT_WPF/Views/Shared/Components/GiaoVien/Controller/LopHocPhanTableView.xaml.cs
@@ -0,0 +1,72 @@
+﻿using QLDT_WPF.Models;
+using QLDT_WPF.Views.Components;
+using Syncfusion.UI.Xaml.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QLDT_WPF.Views.Shared.Components.GiaoVien.View
+{
+    /// <summary>
+    /// Interaction logic for LopHocPhanTableView.xaml
+    /// </summary>
+    public partial class LopHocPhanTableView : UserControl
+    {
+        public ContentControl TargetContentArea
+        {
+            get { return (ContentControl)GetValue(TargetContentAreaProperty); }
+            set { SetValue(TargetContentAreaProperty, value); }
+        }
+
+        public static readonly DependencyProperty TargetContentAreaProperty =
+             DependencyProperty.Register(nameof(TargetContentArea), typeof(ContentControl), typeof(LopHocPhanTableView), new PropertyMetadata(null));
+        public LopHocPhanTableView()
+        {
+            InitializeComponent();
+            Loaded += async (sender, e) =>
+            {
+                if (TargetContentArea == null)
+                {
+                    var parentWindow = FindParent<Window>(this); // Tìm parent window
+                    if (parentWindow != null)
+                    {
+                        var contentArea = parentWindow.FindName("ContentArea") as ContentControl; // Tìm ContentArea
+                        if (contentArea != null)
+                        {
+                            TargetContentArea = contentArea;
+                        }
+                    }
+                }
+            };
+        }
+        private T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            if (parentObject == null) return null;
+
+            if (parentObject is T parent)
+                return parent;
+
+            return FindParent<T>(parentObject);
+        }
+
+        private void dataGridGiaoVien_CellTapped(object sender, GridCellTappedEventArgs e)
+        {
+            var teacherDetails = new LopHocPhanDetailsView(TargetContentArea);
+
+            TargetContentArea.Content = teacherDetails;
+        }
+    }
+}
