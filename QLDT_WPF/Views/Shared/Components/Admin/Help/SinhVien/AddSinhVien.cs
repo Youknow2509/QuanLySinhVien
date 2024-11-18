f825eb9c239c497801e412b7da17e2d0b23cd3db
V <lytranvinh.work@gmail.com>
2024-11-18 16:58:53 +0700

    [upgrade]: handle add sinh vien window helper

--- QLDT_WPF/Views/Shared/Components/Admin/Help/SinhVien/AddSinhVien.cs
+++ QLDT_WPF/Views/Shared/Components/Admin/Help/SinhVien/AddSinhVien.cs
@@ -22,13 +22,26 @@ namespace QLDT_WPF.Views.Shared.Components.Admin.Help
     public partial class AddSinhVien : Window
     {
         // Variables
+        private KhoaRepository khoaRepository;
+        private ChuongTrinhHocRepository chuongTrinhHocRepository;
+        private IdentityRepository identityRepository;
+
+        private List<KhoaDto> khoaDtos;
+        private List<ChuongTrinhHocDto> chuongTrinhHocDtos;
 
         // Constructor
         public AddSinhVien()
         {
             InitializeComponent();
 
-            // init select box khoa
+            khoaRepository = new KhoaRepository();
+            chuongTrinhHocRepository = new ChuongTrinhHocRepository();
+            identityRepository = new IdentityRepository();
+
+            khoaDtos = new List<KhoaDto>();
+            chuongTrinhHocDtos = new List<ChuongTrinhHocDto>();
+
+            // init 
             Loaded += async (sender, e) =>
             {
                 await InitAsync();
@@ -38,7 +51,46 @@ namespace QLDT_WPF.Views.Shared.Components.Admin.Help
         // Init window asynchronously
         private async Task InitAsync()
         {
-            
+            // Load cbbKhoa
+            var req_khoa = await khoaRepository.GetAll();
+            if (req_khoa.Status == false)
+            {
+                MessageBox.Show(req_khoa.Message);
+                return;
+            }
+            khoaDtos = req_khoa.Data;
+            foreach (var khoa in khoaDtos)
+            {
+                cbbKhoa.Items.Add(
+                    new ComboBoxItem
+                    {
+                        Content = khoa.TenKhoa,
+                        Tag = khoa.IdKhoa
+                    }
+                );
+            }
+
+            // Load cbbChuongTrinhHoc
+            var req_chuongTrinhHoc = await chuongTrinhHocRepository.GetAll();
+            if (req_chuongTrinhHoc.Status == false)
+            {
+                MessageBox.Show(req_chuongTrinhHoc.Message);
+                return;
+            }
+            chuongTrinhHocDtos = req_chuongTrinhHoc.Data;
+            foreach (var chuongTrinhHoc in chuongTrinhHocDtos)
+            {
+                cbbChuongTrinhHoc.Items.Add(
+                    new ComboBoxItem
+                    {
+                        Content = chuongTrinhHoc.TenChuongTrinhHoc,
+                        Tag = chuongTrinhHoc.IdChuongTrinhHoc
+                    }
+                );
+            }
+
+            // load time now
+            dpNgaySinh.SelectedDate = DateTime.Now;
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -48,13 +100,57 @@ namespace QLDT_WPF.Views.Shared.Components.Admin.Help
 
         private void CreateButton_Click(object sender, RoutedEventArgs e)
         {
-            // TODO 
-        }
+            string tenSinhVien = txtTenSinhVien.Text.Trim();
+            string maSinhVien = txtMaSinhVien.Text.Trim();
+            string lop = txtLop.Text.Trim();
+            DateTime? ngaySinh = dpNgaySinh.SelectedDate;
+            string idKhoa = (cbbKhoa.SelectedItem as ComboBoxItem)?.Tag.ToString();
+            string idChuongTrinhHoc = (cbbChuongTrinhHoc.SelectedItem as ComboBoxItem)?.Tag.ToString();
 
-        // Only allow integer input in text box
-        private void handle_input_key_press_number(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !int.TryParse(e.Text, out _);
+            if (idKhoa == "-1" || idChuongTrinhHoc == "-1" || tenSinhVien == "" || maSinhVien == "" || lop == "")
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin");
+                return;
+            }
+
+            // Create new SinhVien
+            SinhVienDto newSinhVien = new SinhVienDto
+            {
+                IdSinhVien = maSinhVien,
+                HoTen = tenSinhVien,
+                Lop = lop,
+                NgaySinh = ngaySinh,
+                IdKhoa = idKhoa,
+                IdChuongTrinhHoc = idChuongTrinhHoc
+            };
+
+            // Call API to create new SinhVien
+            try
+            {
+                // Sử dụng Task.Run để chạy hàm bất đồng bộ và đợi kết quả
+                var response = Task.Run(async () => 
+                    await identityRepository.CreateSinhVienUser(newSinhVien, "123456789")
+                ).Result;
+
+                // Kiểm tra kết quả trả về
+                if (response.Status == true)
+                {
+                    MessageBox.Show(response.Message, "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Close(); // Đóng cửa sổ nếu thêm thành công
+                }
+                else
+                {
+                    MessageBox.Show(response.Message, "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (AggregateException ex)
+            {
+                // Xử lý ngoại lệ bất đồng bộ
+                foreach (var innerEx in ex.InnerExceptions)
+                {
+                    MessageBox.Show($"Có lỗi xảy ra: {innerEx.Message}", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
