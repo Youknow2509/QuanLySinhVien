7a7059dc068fa9ef5b9153edcb7490e3d49dbd34
Ly Tran Vinh <lytranvinh.work@gmail.com>
2024-11-22 17:36:58 -0800

    [upgrade]: add colum trang thai nhap diem lop hoc phan

---
+++ QLDT_WPF/Migrations/QuanLySinhVienDb/20241122103405_them_trang_thai_nhap_diem_lop_hoc_phan.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QLDT_WPF.Migrations.QuanLySinhVienDb
+{
+    /// <inheritdoc />
+    public partial class them_trang_thai_nhap_diem_lop_hoc_phan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "TrangThaiNhapDiem",
+                table: "LopHocPhan",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrangThaiNhapDiem",
+                table: "LopHocPhan");
+        }
+    }
+}
